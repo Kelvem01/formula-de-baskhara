225cce313b2c34170c8f072f689ed22230b2eb5e
Kelvem01 <kelvemdasilva16@gmail.com>
2023-06-06 17:59:33 -0300

    adcionando comentarios de boas praticas

--- RH_HOTEL/Crud/programa_principal.py
+++ RH_HOTEL/Crud/programa_principal.py
@@ -1,9 +1,13 @@
 # Kelvem ferreira da silva
 # Fabio Fernandes Elias
 # Trabalho crud umc
+
+# impostrando sql para obter o banco de dados
 import sqlite3
 from funcoes_crud import *
 conn = sqlite3.connect('Crud_umc.sqlite3')
+
+# mostrando as opções do programa
 print("""
     [1] Realizar cadastro
     [2] mostrar cadastro POR ID 
@@ -12,6 +16,7 @@ print("""
     [5] Deletar cadastro
     [0] Finalizar programa """)
 
+# looping com while para melor desenpenho
 while True:
     op = input('Digite a opção desejada: ')
     if op == '0':
@@ -27,6 +32,8 @@ while True:
         atualizar_cad(conn)
     elif op =='5':
         deletar_cad(conn)
-
+    else:
+        print('opção invalida! ')
+# comitando e fechando a conecção 
     conn.commit()
     conn.close()
